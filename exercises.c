fe3f480c32f0456486214b9ef21c02aa8743d7bc
Replit user <>
2022-08-15 00:29:32 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -36,7 +36,7 @@ Utilice la función sumaN.
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
 int array[m];
-  int c=n-m+1;
+  int c=n-m;
     for (int i = c; i<n;i++){
       array[i]=a[i];
   }
